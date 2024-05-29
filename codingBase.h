f316bc692cea063e71ba9ccfeca58c7af3cc8329
InnetaSh <inneta3@gmail.com>
2024-05-29 09:33:04 +0300

    Add virtual class

--- codingBase.h
+++ codingBase.h
@@ -4,5 +4,11 @@
 using namespace std;
 class CodingText
 {
-
+protected:
+	string Text;
+	int key;
+public:
+	CodingText(string str, int n) :Text(str), key(n) {};
+	virtual string Coding(string, int) = 0;
+	virtual string Decoding(string, int) = 0;
 };
