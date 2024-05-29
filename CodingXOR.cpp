8e6151e2fd86068bd39665ea7f3ea8f8446690b3
InnetaSh <inneta3@gmail.com>
2024-05-29 09:43:35 +0300

    function implementation class CodingXOR

--- CodingXOR.cpp
+++ CodingXOR.cpp
@@ -1,1 +1,19 @@
 #include "CodingXOR.h"
+
+string CodingXOR::Coding(string str, int key)
+{
+    string newText = "";
+    for (int i = 0; i < str.size(); ++i) {
+        newText += str[i] ^ key;
+    }
+    return newText;
+}
+
+string CodingXOR::Decoding(string str, int key)
+{
+    string originText = "";
+    for (int i = 0; i < str.size(); ++i) {
+        originText += str[i] ^ key;
+    }
+    return originText;
+}
